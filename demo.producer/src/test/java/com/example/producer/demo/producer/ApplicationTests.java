b39114b74b499a5852b9fd604949bb8e8ea3f6cc
sachinnazare <sachinnazare58@gmail.com>
2025-10-04 13:09:28 +0530

    adding test to producer

--- demo.producer/src/test/java/com/example/producer/demo/producer/ApplicationTests.java
+++ demo.producer/src/test/java/com/example/producer/demo/producer/ApplicationTests.java
@@ -1,13 +1,22 @@
 package com.example.producer.demo.producer;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ApplicationTests {
 
+
+	@InjectMocks
+	kafkaProducerService kafkaProducerService;
+
 	@Test
 	void contextLoads() {
 	}
 
+	@Test
+	void sendMessage(){
+		kafkaProducerService.sendMessage("Sachin");
+	}
 }
