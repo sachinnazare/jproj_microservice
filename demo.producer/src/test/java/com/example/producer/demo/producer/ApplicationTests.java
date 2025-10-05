8bd055b16610c0b1fb961db68e6516ba3723fb1b
sachinnazare <sachinnazare58@gmail.com>
2025-10-05 17:27:34 +0530

    adding test

--- demo.producer/src/test/java/com/example/producer/demo/producer/ApplicationTests.java
+++ demo.producer/src/test/java/com/example/producer/demo/producer/ApplicationTests.java
@@ -2,21 +2,25 @@ package com.example.producer.demo.producer;
 
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.kafka.core.KafkaTemplate;
 
 @SpringBootTest
 class ApplicationTests {
 
+	@Autowired
+	private KafkaTemplate<String, String> kafkaTemplate;
 
-	@InjectMocks
-	kafkaProducerService kafkaProducerService;
+	String topic = "demo-topic";
 
 	@Test
 	void contextLoads() {
 	}
 
 	@Test
-	void sendMessage(){
-		kafkaProducerService.sendMessage("Sachin");
+	void sendMessage() {
+		kafkaTemplate.send(topic, "Sachin");
 	}
 }
