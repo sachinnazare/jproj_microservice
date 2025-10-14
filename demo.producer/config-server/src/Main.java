21d33c69813e26bf7cb32f80e7c08eae99ff3004
sachinnazare <sachinnazare58@gmail.com>
2025-10-14 12:20:14 +0530

    adding config

---
+++ demo.producer/config-server/src/Main.java
@@ -0,0 +1,10 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
+}
